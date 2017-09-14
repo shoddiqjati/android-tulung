25f42d0fbcbe1ec5e7888ca9126ca53cc67c1888
Shoddiq Jati Premono <shoddiq.jati@gmail.com>
2017-09-14 15:11:39 +0700

    Tulung init

---
+++ app/src/main/java/com/onevest/dev/tulung/fragments/TimelineFragment.java
@@ -0,0 +1,25 @@
+package com.onevest.dev.tulung.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Shoddiq Jati Premono on 14/09/2017.
+ */
+
+public class TimelineFragment extends Fragment {
+
+    public TimelineFragment() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
